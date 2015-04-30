423719fb2c7fa96d4864bb7fa9dfeb72f2b4ad98
Derik Pell <dpell@myemma.com>
2015-04-29 22:58:03 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# My stolen evil emacs files
+I borrowed very heavily from a few sources for this. I'm linking to  their repos below so anyone that finds this can get to the original sources.
+I had to make some changes to accomodate how I wanted things and for updated packages, but the core is still the same.
+
+
+## From Daniel Higginbotham's Clojure for the Brave and True [repo](https://github.com/flyingmachine/emacs-for-clojure)
+You should definitely check out his [Clojure for the Brave and True](http://www.braveclojure.com/). It's a great intro to clojure. 
+
+## From Drew Warner's [repo](https://github.com/wernerandrew/jedi-starter)
+He has a really good youtube video explaining this setup [here](https://www.youtube.com/watch?v=6BlTGPsjGJk)
+
+I also use evil mode because vim is a great text editor. I set the leader key to SPC because I think spacemacs got that right, but that package was just a bit too bloated for me.
---
+++ init.el
@@ -0,0 +1,235 @@
+
+;; Define package repositories
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+             '("tromey" . "http://tromey.com/elpa/") t)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+;; (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+;;                          ("marmalade" . "http://marmalade-repo.org/packages/")
+;;                          ("melpa" . "http://melpa-stable.milkbox.net/packages/")))
+
+
+;; Load and activate emacs packages. Do this first so that the
+;; packages are loaded before you start trying to modify them.
+;; This also sets the load path.
+(package-initialize)
+
+;; Download the ELPA archive description if needed.
+;; This informs Emacs about the latest versions of all packages, and
+;; makes them available for download.
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; The packages you want installed. You can also install these
+;; manually with M-x package-install
+;; Add in your own as you wish:
+(defvar my-packages
+  '(;; makes handling lisp expressions much, much easier
+    ;; Cheatsheet: http://www.emacswiki.org/emacs/PareditCheatsheet
+    paredit
+
+    ;; key bindings and code colorization for Clojure
+    ;; https://github.com/clojure-emacs/clojure-mode
+    clojure-mode
+
+    ;; extra syntax highlighting for clojure
+    clojure-mode-extra-font-locking
+
+    ;; integration with a Clojure REPL
+    ;; https://github.com/clojure-emacs/cider
+    cider
+
+    ;; allow ido usage in as many contexts as possible. see
+    ;; customizations/navigation.el line 23 for a description
+    ;; of ido
+    ido-ubiquitous
+
+    ;; Enhances M-x to allow easier execution of commands. Provides
+    ;; a filterable list of possible commands in the minibuffer
+    ;; http://www.emacswiki.org/emacs/Smex
+    smex
+
+    ;; project navigation
+    projectile
+
+    ;; colorful parenthesis matching
+    rainbow-delimiters
+
+    ;; edit html tags like sexps
+    tagedit
+
+    ;; git integration
+    magit
+    2048-game          
+    async              
+    auto-complete      
+    clojure-mode-extra-font-locking
+    concurrent         
+    ctable             
+    cyberpunk-theme    
+    dash               
+    deferred           
+    epc                
+    epl                
+    evil               
+    evil-escape        
+    evil-leader        
+    evil-org           
+    evil-paredit       
+    exec-path-from-shell 
+    flycheck           
+    git-commit-mode    
+    git-rebase-mode    
+    goto-chg           
+    helm               
+    helm-ag            
+    helm-projectile    
+    ido-completing-read+ 
+    jedi               
+    jedi-core          
+    let-alist          
+    linum-relative     
+    org                
+    pkg-info           
+    popup              
+    python-environment 
+    queue              
+    s                  
+    undo-tree          
+))
+
+
+;; OS X, an Emacs instance started from the graphical user
+;; interface will have a different environment than a shell in a
+;; terminal window, because OS X does not run a shell during the
+;; login. Obviously this will lead to unexpected results when
+;; calling external utilities like make from Emacs.
+;; This library works around this problem by copying important
+;; environment variables from the user's shell.
+;; https://github.com/purcell/exec-path-from-shell
+(if (eq system-type 'darwin)
+    (add-to-list 'my-packages 'exec-path-from-shell))
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+
+;; Place downloaded elisp files in ~/.emacs.d/vendor. You'll then be able
+;; to load them.
+;;
+;; For example, if you download yaml-mode.el to ~/.emacs.d/vendor,
+;; then you can add the following code to this file:
+;;
+;; (require 'yaml-mode)
+;; (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+;; 
+;; Adding this code will make Emacs enter yaml mode whenever you open
+;; a .yml file
+(add-to-list 'load-path "~/.emacs.d/vendor")
+
+
+;;;;
+;; Customization
+;;;;
+
+;; Add a directory to our load path so that when you `load` things
+;; below, Emacs knows where to look for the corresponding file.
+(add-to-list 'load-path "~/.emacs.d/customizations")
+
+;; Sets up exec-path-from-shell so that Emacs will use the correct
+;; environment variables
+(load "shell-integration.el")
+
+;; jedi server
+(load "jedi-starter.el")
+
+;; These customizations make it easier for you to navigate files,
+;; switch buffers, and choose options from the minibuffer.
+(load "navigation.el")
+
+;; These customizations change the way emacs looks and disable/enable
+;; some user interface elements
+(load "ui.el")
+
+;; These customizations make editing a bit nicer.
+(load "editing.el")
+
+;; Hard-to-categorize customizations
+(load "misc.el")
+
+;; For editing lisps
+(load "elisp-editing.el")
+
+;; Langauage-specific
+(load "setup-clojure.el")
+(load "setup-js.el")
+
+;;my stuff
+(require 'evil-leader)
+(global-evil-leader-mode)
+(require 'evil)
+(evil-mode 1)
+(load "~/.emacs.d/vendor/jedi-starter.el")
+(require 'jedi)
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+(flymake-mode)
+;;evil
+(evil-leader/set-leader "<SPC>")
+(evil-leader/set-key 
+ "b" 'switch-to-buffer
+ "pf" 'projectile-find-file
+ "ag" 'helm-projectile-ag
+ "s" 'save-buffer
+ ;;org mode
+  "oc" 'org-cycle
+  ;;projectile
+  "pf" 'projectile-find-file
+)
+
+;;default theme
+(load-theme 'cyberpunk t)
+
+;;keep autosave files in tmp dir
+(setq backup-directory-alist
+          `((".*" . ,temporary-file-directory)))
+    (setq auto-save-file-name-transforms
+          `((".*" ,temporary-file-directory t)))
+
+(require 'linum-relative)
+
+(setq-default evil-escape-key-sequence "jk")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(coffee-tab-width 2)
+ '(custom-safe-themes
+   (quote
+    ("90edd91338ebfdfcd52ecd4025f1c7f731aced4c9c49ed28cfbebb3a3654840b" "9e54a6ac0051987b4296e9276eecc5dfb67fdcd620191ee553f40a9b6d943e78" default)))
+ '(global-flycheck-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; copy/paste
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx)
